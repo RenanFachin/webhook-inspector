41366fac1d90adf7af32f6aa55470a4a9c81c785
Renan <renan.fachin@hotmail.com>
2025-11-22 23:56:05 -0300

    feat: Enhance listWebhooks route to support pagination with cursor; update response schema to include pathname and createdAt fields

--- api/src/routes/list-webhooks.ts
+++ api/src/routes/list-webhooks.ts
@@ -1,3 +1,7 @@
+import { db } from '@/db'
+import { webhooks } from '@/db/schema'
+import { desc, lt } from 'drizzle-orm'
+import { createSelectSchema } from 'drizzle-zod'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 
@@ -11,26 +15,41 @@ export const listWebhooks: FastifyPluginAsyncZod = async (app) => {
         tags: ["webhooks"],
         querystring: z.object({
           limit: z.coerce.number().min(1).max(100).default(20),
+          cursor: z.string().optional(),
         }),
         response: {
-          200: z.array(
-            z.object({
-              id: z.string(),
-              method: z.string(),
-            })
-          )
+          200: z.object({
+            webhooks: z.array(
+              createSelectSchema(webhooks).pick({
+                id: true,
+                method: true,
+                pathname: true,
+                createdAt: true
+              })
+            ),
+            nextCursor: z.string().nullable(),
+          })
         }
       }
     },
     async (request, reply) => {
-      const { limit } = request.query
+      const { limit, cursor } = request.query
 
+      const result = await db.select({
+        id: webhooks.id,
+        method: webhooks.method,
+        pathname: webhooks.pathname,
+        createdAt: webhooks.createdAt,
+      }).from(webhooks).where(cursor ? lt(webhooks.id, cursor) : undefined).orderBy(desc(webhooks.id)).limit(limit + 1)
 
-      return [
-        {
-          id: '1',
-          method: 'POST',
-        }
-      ]
+      // exemplo, se retornar 21, ou seja, maior que os 20 do limit, então tem mais
+      const hasMore = result.length > limit
+      const items = hasMore ? result.slice(0, limit) : result
+      const nextCursor = hasMore ? items[items.length - 1].id : null
+
+      return reply.send({
+        webhooks: items,
+        nextCursor,
+      })
     })
 }
